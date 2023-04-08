fe203fba3666bc6116f5ac95dc183e14a0217b2d
abhsingh487 <86626295+abhsingh487@users.noreply.github.com>
2023-04-08 05:53:08 +0000

    Initial view of the 'Transaction' page

---
+++ cash_crusher_rjs/src/App.js
@@ -0,0 +1,12 @@
+import { Stack } from "react-bootstrap"
+import Container from "react-bootstrap/Container"
+
+function App() {
+  return  <Container>
+    <Stack direction = "horizontal" gap="2" className="mb-4">
+      <h1>Transactions</h1>
+    </Stack>
+  </Container>
+}
+
+export default App;

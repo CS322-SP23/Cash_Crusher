fbd08ac91e428ead47639951f9ae47802612a53d
abhsingh487 <86626295+abhsingh487@users.noreply.github.com>
2023-04-08 06:36:10 +0000

    Completed the 'Transaction' and the 'Date' branch for the page.

--- cash_crusher_rjs/src/App.js
+++ cash_crusher_rjs/src/App.js
@@ -1,11 +1,22 @@
 import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import React, { useState } from "react";
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
     <Container>
       <Stack direction="horizontal" gap="2" className="mb-4 justify-content-center bg-success">
         <h1 className="text-white p-3">Transactions</h1>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+        />
         <Button variant="primary">Refresh</Button> 
       </Stack>
     </Container>

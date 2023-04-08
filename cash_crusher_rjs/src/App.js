6e18965b9d3584de0fde1dbad66eadfed2027458
abhsingh487 <86626295+abhsingh487@users.noreply.github.com>
2023-04-08 06:03:21 +0000

    Centrally aligned word 'Transaction' with a 'Refresh' button

--- cash_crusher_rjs/src/App.js
+++ cash_crusher_rjs/src/App.js
@@ -1,12 +1,15 @@
-import { Stack } from "react-bootstrap"
-import Container from "react-bootstrap/Container"
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return  <Container>
-    <Stack direction = "horizontal" gap="2" className="mb-4">
-      <h1>Transactions</h1>
-    </Stack>
-  </Container>
+  return (
+    <Container>
+      <Stack direction="horizontal" gap="2" className="mb-4 justify-content-center bg-success">
+        <h1 className="text-white p-3">Transactions</h1>
+        <Button variant="primary">Refresh</Button> 
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;

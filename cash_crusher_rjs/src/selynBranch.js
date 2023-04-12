c61f9595be149d0689db74c2f182e6a8cca93c67
SelynLee <63301685+SelynLee@users.noreply.github.com>
2023-04-12 22:14:39 +0000

    created weekly, monthly, and summary tabs

---
+++ cash_crusher_rjs/src/selynBranch.js
@@ -0,0 +1,21 @@
+import Nav from 'react-bootstrap/Nav';
+
+function TabsExample() {
+  return (
+    <Nav variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Daily</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Monthly</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="Summary" Summary>
+          Summary
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
+
+export default TabsExample;

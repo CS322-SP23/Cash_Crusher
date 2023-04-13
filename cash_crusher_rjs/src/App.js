fb6282eebff629240882c2670c28bc91fd337041
Jamari Mitchell <92458788+yaboijams@users.noreply.github.com>
2023-04-13 02:26:44 +0000

    Account Section Moved to Right side

--- cash_crusher_rjs/src/App.js
+++ cash_crusher_rjs/src/App.js
@@ -14,6 +14,36 @@ function App() {
         </Col>
       </Row>
       <Row className="h-100">
+        <Col xs={12} md={9} className="bg-light p-5">
+          <Row className="mb-4">
+            <Col>
+              <h2 className="mb-3">Transaction History</h2>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="MM/yyyy"
+                showMonthYearPicker
+                className="form-control"
+              />
+            </Col>
+            <Col className="text-end">
+              <Button variant="primary">Refresh</Button>
+            </Col>
+          </Row>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Put transaction here. You copied the format to a sticky note on your computer */}
+            </tbody>
+          </Table>
+        </Col>
         <Col xs={12} md={3} className="bg-light py-5">
           <h3 className="text-center mb-4">Accounts</h3>
           <ul className="list-unstyled">
@@ -33,59 +63,6 @@ function App() {
               </a>
             </li>
           </ul>
-        </Col>
-        <Col xs={12} md={9} className="bg-light p-5">
-          <Row className="mb-4">
-            <Col>
-              <h2 className="mb-3">Transaction History</h2>
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                dateFormat="MM/yyyy"
-                showMonthYearPicker
-                className="form-control"
-              />
-            </Col>
-            <Col className="text-end">
-              <Button variant="primary">Download</Button>
-            </Col>
-          </Row>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Description</th>
-                <th>Category</th>
-                <th>Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>04/10/2023</td>
-                <td>Amazon.com</td>
-                <td>Shopping</td>
-                <td className="text-danger">-$49.99</td>
-              </tr>
-              <tr>
-                <td>04/09/2023</td>
-                <td>Paycheck</td>
-                <td>Salary</td>
-                <td className="text-success">$2,500.00</td>
-              </tr>
-              <tr>
-                <td>04/08/2023</td>
-                <td>Starbucks</td>
-                <td>Coffee</td>
-                <td className="text-danger">-$4.99</td>
-              </tr>
-              <tr>
-                <td>04/07/2023</td>
-                <td>Uber</td>
-                <td>Transportation</td>
-                <td className="text-danger">-$15.00</td>
-              </tr>
-            </tbody>
-          </Table>
         </Col>
       </Row>
     </Container>

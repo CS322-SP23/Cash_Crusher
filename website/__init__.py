a2c2b80129e5782a48a92ee7090ba7cfd76d65e7
abhsingh487 <86626295+abhsingh487@users.noreply.github.com>
2023-04-20 16:54:35 +0000

    The Login Page for the Cash Crusher App

--- website/__init__.py
+++ website/__init__.py
@@ -1,23 +1,13 @@
-"""
-This module contains a Flask application for the Cash Crusher website.
-
-"""
-
 from flask import Flask
 
 def create_app():
-    """
-    Creates a Flask application for the Cash Crusher website.
- 
-    :return: A Flask app object with a secret key set in its configuration.
-    """
-    app = Flask(__name__)
-    app.config['SECRET KEY'] = 'Cash_Crusher_ASJ'
-
+    app=Flask(__name__)
+    app.config['SECRET_KEY'] = 'Cash_Crusher'
+    
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
+    
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-auth=Blueprint('auth',__name__)
+auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Signup</p>"
+    return render_template("signup.html")

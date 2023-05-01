c7c83415dfdf321c2c5a0d86b9e2f60c75845b55
SelynLee <63301685+SelynLee@users.noreply.github.com>
2023-05-01 11:03:03 +0000

    created whole calendar

--- cash_crusher_rjs/src/pages/Calendar.js
+++ cash_crusher_rjs/src/pages/Calendar.js
@@ -1,6 +1,59 @@
 import React, { Component } from "react";
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { format, addMonths, subMonths } from 'date-fns';
+import { initializeApp, getApp } from "firebase/app";
+import firebaseConfig from '../firebase';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import "firebase/auth";
 
+let firebaseApp;
 
+const RenderHeader = ({ currentMonth, prevMonth, nextMonth }) => {
+  return (
+    <div className="header row">
+      <div className="col col-start">
+        <span className="text">
+          <span className="text month">
+            {format(currentMonth, 'M')}Month
+          </span>
+          {format(currentMonth, 'yyyy')}
+        </span>
+      </div>
+      <div className="col col-end">
+        <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
+        <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
+      </div>
+    </div>
+  );
+};
+
+const Calendar = () => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [selectedData, setSelectedDate] = useState(new Date());
+
+  const prevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+  const nextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+  return (
+    <div className="calendar">
+      <RenderHeader
+        currentMonth={currentMonth}
+        prevMonth={prevMonth}
+        nextMonth={nextMonth}
+      />
+      <div className="days">Days</div>
+      <div className="body">Cells</div>
+    </div>
+  );
+
+}
+export default Calendar;
+
+/*
 class Calendar extends React.Component{
     constructor(props) {
       super();
@@ -18,3 +71,4 @@ class Calendar extends React.Component{
   }
   
   export default Calendar;
+  */

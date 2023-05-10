7973dd7d8e57de5c98fd4464b3d1bb181368ed24
SelynLee <63301685+SelynLee@users.noreply.github.com>
2023-05-10 01:48:01 +0000

    connected the popup page to the Firebase DB

--- cash_crusher_rjs/src/pages/ModalComponent.js
+++ cash_crusher_rjs/src/pages/ModalComponent.js
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import CustomModal from './CustomModal';
 import Button from 'react-bootstrap/Button';
 import { format } from 'date-fns';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { Timestamp } from 'firebase/firestore';
 
-const ModalComponent = ({ modalData, handleClose, show, selectedDate }) => {
+const ModalComponent = ({ handleClose, show, selectedDate }) => {
+  const [modalData, setModalData] = useState(null);
+
+  useEffect(() => {
+    if (selectedDate) {
+      const startDate = Timestamp.fromDate(selectedDate);
+      const endDate = Timestamp.fromMillis(selectedDate.getTime() + 24 * 60 * 60 * 1000);
+      const db = getFirestore();
+      const q = query(
+        collection(db, 'Transactions'),
+        where('date', '>=', startDate),
+        where('date', '<', endDate)
+      );
+      getDocs(q).then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        setModalData(data);
+      });
+    }
+  }, [selectedDate]);
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-      <Modal.Title>{selectedDate ? `Transaction History ${format(selectedDate, '(MMMM do, yyyy)')}` : 'Popup page'}</Modal.Title>
+        <Modal.Title>
+          {selectedDate ? `Transaction History ${format(selectedDate, '(MMMM do, yyyy)')}` : 'Popup page'}
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         {modalData && modalData.length > 0 ? (
@@ -42,25 +64,5 @@ const ModalComponent = ({ modalData, handleClose, show, selectedDate }) => {
     </Modal>
   );
 };
-export default ModalComponent;
-/*
-const ModalComponent = ({ show, handleClose, selectedDate }) => {
-    return (
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{selectedDate ? `Transaction History ${format(selectedDate, '(MMMM do, yyyy)')}` : 'Popup page'}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>This is the content of the popup page.</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  };  
 
 export default ModalComponent;
-*/

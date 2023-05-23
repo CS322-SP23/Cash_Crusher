8d0444277b2a3b5263d0398076025ef87c77f10a
Jamari Mitchell <92458788+yaboijams@users.noreply.github.com>
2023-05-23 23:46:13 +0000

    working app 5_23_23

--- cash_crusher_rjs/src/firebase.js
+++ cash_crusher_rjs/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPzZ2rN_s83GbzQQmh8O1I9B45Qd0df8Q",
-  authDomain: "cashcrusher-894c1.firebaseapp.com",
-  projectId: "cashcrusher-894c1",
-  storageBucket: "cashcrusher-894c1.appspot.com",
-  messagingSenderId: "429380298469",
-  appId: "1:429380298469:web:9fc7394022791954ae960c",
-  measurementId: "G-8CVJFVRT5R"
+  apiKey: "AIzaSyBLq_1JSwkj1EI0d5u48fvKg0wBEHcBz5o",
+  authDomain: "cashcrusher2.firebaseapp.com",
+  projectId: "cashcrusher2",
+  storageBucket: "cashcrusher2.appspot.com",
+  messagingSenderId: "139936473493",
+  appId: "1:139936473493:web:0c765e4ab82123a0ea169c",
+  measurementId: "G-W3GWESNH1W"
 };
 
 firebase.initializeApp(firebaseConfig);

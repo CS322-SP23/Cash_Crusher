f8ac74916a0230d53d3b7d17528639fb7bc9a2e5
abhsingh487 <86626295+abhsingh487@users.noreply.github.com>
2023-05-25 08:53:20 +0000

    Updated firebase.js

--- cash_crusher_rjs/src/firebase.js
+++ cash_crusher_rjs/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPzZ2rN_s83GbzQQmh8O1I9B45Qd0df8Q",
-  authDomain: "cashcrusher-894c1.firebaseapp.com",
-  projectId: "cashcrusher-894c1",
-  storageBucket: "cashcrusher-894c1.appspot.com",
-  messagingSenderId: "429380298469",
-  appId: "1:429380298469:web:9fc7394022791954ae960c",
-  measurementId: "G-8CVJFVRT5R"
+  apiKey: "AIzaSyAE0-Is3VEJQjK0ysKmOv4c44XYxEA08oo",
+  authDomain: "cashcrusherbudgetapp.firebaseapp.com",
+  projectId: "cashcrusherbudgetapp",
+  storageBucket: "cashcrusherbudgetapp.appspot.com",
+  messagingSenderId: "966516487265",
+  appId: "1:966516487265:web:c6e2378f2aeca561b52620",
+  measurementId: "G-N6Y1CJ03Y1"
 };
 
 firebase.initializeApp(firebaseConfig);
